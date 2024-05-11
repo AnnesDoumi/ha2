885ee3f4695db48c65109d84d1f261ce6687e393
Annes Bani Al Doumi <annesdoumi2@gmail.com>
2024-05-12 01:49:10 +0200

    PreComputedBurger

---
+++ app/src/main/java/htw/berlin/wi/prog2/domain/PreComputedBurger.java
@@ -0,0 +1,30 @@
+package htw.berlin.wi.prog2.domain;
+
+import java.util.List;
+
+public class PreComputedBurger implements Burger {
+    private final double price;
+    private final int calories;
+    private final List<String> ingredientNames;
+
+    public PreComputedBurger(double price, int calories, List<String> ingredientNames) {
+        this.price = price;
+        this.calories = calories;
+        this.ingredientNames = ingredientNames;
+    }
+
+    @Override
+    public double calculatePrice() {
+        return this.price;
+    }
+
+    @Override
+    public int calculateCalories() {
+        return this.calories;
+    }
+
+    @Override
+    public List<String> getIngredientNames() {
+        return this.ingredientNames;
+    }
+}
